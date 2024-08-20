9f7c9412bdcbd04ac4d4dd32c9f12821a30c33d7
govinda <govinda@gmail.com>
2024-08-20 09:56:33 +0530

    change

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
-/// <reference types="vite/client" />
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-      proxy: {
-        '/api': {
-          target: 'https://hospital-management-system-h9yz.onrender.com',
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''),
-        },
-      },
-    },
-});
+})
